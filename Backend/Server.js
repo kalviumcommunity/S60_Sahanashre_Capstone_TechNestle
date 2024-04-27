beb7f3306ebc8d4cae43ed543df947c2ebd2031f
V.Sahanashre <sahanashre.v@kalvium.community>
2024-04-27 17:11:21 +0530

    Connected to database

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,10 +1,21 @@
 const Express = require("express")
 const app = Express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGODB_CONNECTION
 
 app.get("/",(req,res)=>{
     res.send("This is TechNestle")
 })
 
-app.listen(8080,()=>{
-    console.log("Server is running on the port 8080")
+mongoose.connect(MONGO_URI)
+.then(()=>{
+    console.log("Database connected successfully")
+    app.listen(PORT,()=>{
+        console.log(`Server is running on the port ${PORT}`)
+    })
 })
+.catch((error)=> console.log("Error in connecting to database",error.message) )
